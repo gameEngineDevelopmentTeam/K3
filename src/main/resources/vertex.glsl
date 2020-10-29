d72fd7ff09c72678654ddc832effaae61ad6c3e7
CYBAiGNU <43635561+Cyberdog90@users.noreply.github.com>
2020-10-29 21:26:54 +0900

    現状動作するやつ

--- src/main/resources/vertex.glsl
+++ src/main/resources/vertex.glsl
@@ -1,12 +1,8 @@
 #version 330
 
 layout (location =0) in vec3 position;
-layout (location =1) in vec3 inColor;
-
-out vec3 exColor;
 
 void main()
 {
-    gl_Position = vec4(position, 1.0);
-    exColor = inColor;
+	gl_Position = vec4(position, 1.0);
 }
