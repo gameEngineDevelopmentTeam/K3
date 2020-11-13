e2b917be29e50dd08d1c1331424639b635bfa772
CYBAiGNU <43635561+Cyberdog90@users.noreply.github.com>
2020-11-13 21:05:12 +0900

    OpenGL4.6 -> OpenGL3.2

--- src/main/java/itoh/engine/Window.kt
+++ src/main/java/itoh/engine/Window.kt
@@ -59,8 +59,8 @@ class Window(private val title: String, private var width: Int, private var heig
         glfwDefaultWindowHints()
         glfwWindowHint(GLFW_VISIBLE, GL_FALSE)
         glfwWindowHint(GLFW_RESIZABLE, GL_TRUE)
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4)
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6)
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3)
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2)
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE)
         glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE)
 
