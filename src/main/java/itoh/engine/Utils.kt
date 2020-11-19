fa0d2cd04f572202c213a1a0f938fb7f7427a1d1
CYBAiGNU <43635561+Cyberdog90@users.noreply.github.com>
2020-11-20 01:33:50 +0900

    ちょっと加えた

--- src/main/java/itoh/engine/Utils.kt
+++ src/main/java/itoh/engine/Utils.kt
@@ -1,5 +1,6 @@
 package itoh.engine
 
+import itoh.engine.polygon.Texture
 import java.io.BufferedInputStream
 import java.io.DataInputStream
 import java.io.File
@@ -14,5 +15,21 @@ class Utils {
                     System.getProperty("user.dir") + "/src/main/resources/$fileName"
             ).readText(Charsets.UTF_8)
         }
+
+        fun loadResourceCR(fileName: String): List<String> {
+            println("Load Resource   : $fileName")
+            var data =  File(
+                    System.getProperty("user.dir") + "/src/main/resources/$fileName"
+            ).readText(Charsets.UTF_8)
+            return data.split("\n")
+        }
     }
+}
+
+/*ex*/
+class TextureEx{
+
+}
+fun Texture.ex(){
+
 }
