69c57133dff328f404e86f74d1f39decb9e81390
Cyberdog90 <43635561+Cyberdog90@users.noreply.github.com>
2020-11-23 11:04:40 +0900

    hogehoge

--- src/main/java/itoh/engine/polygon/ObjLoader.kt
+++ src/main/java/itoh/engine/polygon/ObjLoader.kt
@@ -1,21 +1,42 @@
 package itoh.engine.polygon
 
 import itoh.engine.Resources.Companion.loadResource
+import org.joml.Vector2f
+import org.joml.Vector3f
 
 
 class ObjLoader {
-    //ファイル読み込み
-    private val objData = loadResource("cube.obj").split("\n")
     fun objLoader() {
+        //ファイル読み込み
+        val objData = loadResource("cube.obj").split("\n")
+        val vList: ArrayList<Vector3f> = ArrayList()
+        val vtList: ArrayList<Vector2f> = ArrayList()
+        val vnList: ArrayList<Vector3f> = ArrayList()
+        val fList: ArrayList<Face> = ArrayList()
+
         for (dataLine in objData) {
             val parsed = dataLine.split(" ")
             when (parsed[0]) {
                 // 頂点
                 "v" -> {
+                    vList.add(Vector3f(
+                            parsed[1].toFloat(),
+                            parsed[2].toFloat(),
+                            parsed[3].toFloat()
+                    ))
                 }
                 "vt" -> {
+                    vtList.add(Vector2f(
+                            parsed[1].toFloat(),
+                            parsed[2].toFloat()
+                    ))
                 }
                 "vn" -> {
+                    vnList.add(Vector3f(
+                            parsed[1].toFloat(),
+                            parsed[2].toFloat(),
+                            parsed[3].toFloat()
+                    ))
                 }
                 "f" -> {
                 }
@@ -28,9 +49,7 @@ class ObjLoader {
 
 }
 
-data class Object(
-        val vList: FloatArray = floatArrayOf(),
-        val vtList: FloatArray = floatArrayOf(),
-        val vnList: FloatArray = floatArrayOf(),
-        val fList: IntArray = intArrayOf()
-)
+
+class Face {
+
+}
