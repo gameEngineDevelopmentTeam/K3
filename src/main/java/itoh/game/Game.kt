7c655ba3b3e32411a0751cf5354eaa80a285130d
kyodainamekuji <ciha18089@g.nihon-u.ac.jp>
2020-11-26 10:35:56 +0900

    hoge

--- src/main/java/itoh/game/Game.kt
+++ src/main/java/itoh/game/Game.kt
@@ -28,7 +28,7 @@ open class Game : GameLogic {
 
     override fun initialization(window: Window) {
         renderer.initialization()
-        val mesh = objLoader.objLoader("Negev.obj")
+        val mesh = objLoader.objLoader("bunny.obj")
         val texture = Texture("src/main/resources/whiteImage.png")
         mesh.texture = texture
         val gameItem = Obj3D(mesh)
